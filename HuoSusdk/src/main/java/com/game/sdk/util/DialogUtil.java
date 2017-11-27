a16959647bb11cb4bf1683ff2f3cafe3120453f0
hover <hover_android@126.com>
2017-11-27 18:11:20 +0800

    1.替换悬浮球图片 2.弹出公告测试

--- HuoSusdk/src/main/java/com/game/sdk/util/DialogUtil.java
+++ HuoSusdk/src/main/java/com/game/sdk/util/DialogUtil.java
@@ -22,159 +22,157 @@ import com.game.sdk.domain.Notice;
 @NotProguard
 public class DialogUtil {
 
-	private static Dialog dialog;// 显示对话框
-	private static ImageView iv_pd;// 待旋转动画
-	private static TextView tv_msg;// 消息
-	private static View view;
+    private static Dialog dialog;// 显示对话框
+    private static ImageView iv_pd;// 待旋转动画
+    private static TextView tv_msg;// 消息
+    private static View view;
 
-	/**
-	 * 显示对话框
-	 */
-	private static void init(Context context) {
-		dialog = new Dialog(context, MResource.getIdByName(context, "style",
-				"huo_sdk_customDialog"));
-		view = LayoutInflater.from(context).inflate(
-				MResource.getIdByName(context, "layout", "huo_sdk_dialog_loading"), null);
-		iv_pd = (ImageView) view.findViewById(MResource.getIdByName(context,
-				"id", "huo_sdk_iv_circle"));
-		dialog.setCancelable(false);
-		dialog.setCanceledOnTouchOutside(false);
-		tv_msg = (TextView) view.findViewById(MResource.getIdByName(context,
-				"id", "huo_sdk_tv_msg"));
-		dialog.setContentView(view);
-	}
+    /**
+     * 显示对话框
+     */
+    private static void init(Context context) {
+        dialog = new Dialog(context, MResource.getIdByName(context, "style",
+                "huo_sdk_customDialog"));
+        view = LayoutInflater.from(context).inflate(
+                MResource.getIdByName(context, "layout", "huo_sdk_dialog_loading"), null);
+        iv_pd = (ImageView) view.findViewById(MResource.getIdByName(context,
+                "id", "huo_sdk_iv_circle"));
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+        tv_msg = (TextView) view.findViewById(MResource.getIdByName(context,
+                "id", "huo_sdk_tv_msg"));
+        dialog.setContentView(view);
+    }
 
-	/**
-	 * 显示对话框
-	 * 
-	 * @param context
-	 * @param msg
-	 */
-	public static void showDialog(Context context, String msg) {
-		try {
-			init(context);
-			tv_msg.setText(msg);// 显示进度信息
-			if (null != dialog && !dialog.isShowing()) {
+    /**
+     * 显示对话框
+     *
+     * @param context
+     * @param msg
+     */
+    public static void showDialog(Context context, String msg) {
+        try {
+            init(context);
+            tv_msg.setText(msg);// 显示进度信息
+            if (null != dialog && !dialog.isShowing()) {
                 iv_pd.startAnimation(rotaAnimation());
                 dialog.show();
             }
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void showDialog(Context ctx, boolean cansable, String msg) {
-		if(dialog!=null&&dialog.isShowing()){
-			dialog.dismiss();
-		}
-		init(ctx);
-		tv_msg.setText(msg);// 显示进度信息
-		if (null != dialog && !dialog.isShowing()) {
-			dialog.setCancelable(cansable);
-			iv_pd.startAnimation(rotaAnimation());
-			dialog.show();
-		}
-	}
+    public static void showDialog(Context ctx, boolean cansable, String msg) {
+        if (dialog != null && dialog.isShowing()) {
+            dialog.dismiss();
+        }
+        init(ctx);
+        tv_msg.setText(msg);// 显示进度信息
+        if (null != dialog && !dialog.isShowing()) {
+            dialog.setCancelable(cansable);
+            iv_pd.startAnimation(rotaAnimation());
+            dialog.show();
+        }
+    }
 
-	/**
-	 * 隐藏对话框
-	 */
-	public static void dismissDialog() {
-		try {
-			if (null != dialog && dialog.isShowing()) {
+    /**
+     * 隐藏对话框
+     */
+    public static void dismissDialog() {
+        try {
+            if (null != dialog && dialog.isShowing()) {
                 dialog.dismiss();
                 iv_pd.clearAnimation();
                 dialog = null;
             }
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * 旋转动画
-	 * 
-	 * @return
-	 */
-	public static Animation rotaAnimation() {
-		RotateAnimation ra = new RotateAnimation(0, 355,
-				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
-				0.5f);
-		ra.setInterpolator(new LinearInterpolator());
-		ra.setDuration(888);
-		ra.setRepeatCount(-1);
-		ra.setStartOffset(0);
-		ra.setRepeatMode(Animation.RESTART);
-		return ra;
-	}
+    /**
+     * 旋转动画
+     *
+     * @return
+     */
+    public static Animation rotaAnimation() {
+        RotateAnimation ra = new RotateAnimation(0, 355,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        ra.setInterpolator(new LinearInterpolator());
+        ra.setDuration(888);
+        ra.setRepeatCount(-1);
+        ra.setStartOffset(0);
+        ra.setRepeatMode(Animation.RESTART);
+        return ra;
+    }
 
-	/**
-	 * 判断对话框是否是显示状态
-	 * 
-	 * @return
-	 */
-	public static boolean isShowing() {
-		if (null != dialog) {
-			return dialog.isShowing();
-		}
-		return false;
-	}
+    /**
+     * 判断对话框是否是显示状态
+     *
+     * @return
+     */
+    public static boolean isShowing() {
+        if (null != dialog) {
+            return dialog.isShowing();
+        }
+        return false;
+    }
 
-	/**
-	 * 弹出公告框
-	 * @param notice
-	 */
-	public static void showNoticeDialog(final Context context,final Notice notice){
+    /**
+     * 弹出公告框
+     *
+     * @param notice
+     */
+    public static void showNoticeDialog(final Context context, Notice notice) {
 
-//		final Notice notice = new Notice();
-//		notice.setTitle("测试标题");
-//		notice.setContent("测试内容测试内容测试内容测试内容测试内容");
-//		notice.setTime("2017年1月17日16:02:00");
+        notice = new Notice("游戏公告","游戏公告游戏公告游戏公告游戏公告游戏公告游戏公告游戏公告","2017年11月27日16:02:00");
+        if (notice == null || TextUtils.isEmpty(notice.getContent())) return;
 
-		if (notice == null || TextUtils.isEmpty(notice.getContent())) return;
+        Log.e("notice", " notice.title:" + notice.getTitle() + " | notice.setContent:" + notice.getContent());
+        if (context instanceof Activity) {
 
-		Log.e("notice"," notice.title:"+notice.getTitle()+" | notice.setContent:"+notice.getContent());
-		if (context instanceof Activity){
+            final Notice finalNotice = notice;
+            ((Activity) context).runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
 
-			((Activity) context).runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
+                    final Dialog dialog = new Dialog(context, MResource.getIdByName(context,
+                            "style", "huo_sdk_customDialog"));
+                    View notcieView = LayoutInflater.from(context)
+                            .inflate(MResource.getIdByName(context, "layout", "huo_sdk_dialog_notice"), null);
+                    TextView title = (TextView) notcieView.findViewById(MResource.getIdByName(context,
+                            "id", "huo_sdk_title_text"));
+                    TextView time = (TextView) notcieView.findViewById(MResource.getIdByName(context,
+                            "id", "huo_sdk_time_text"));
+                    TextView content = (TextView) notcieView.findViewById(MResource.getIdByName(context,
+                            "id", "huo_sdk_content_text"));
+                    TextView confirm = (TextView) notcieView.findViewById(MResource.getIdByName(context,
+                            "id", "huo_sdk_confirm_tv"));
+                    title.setText(finalNotice.getTitle());
+                    time.setText(finalNotice.getTime());
+                    content.setText(finalNotice.getContent());
+                    confirm.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (dialog != null) {
+                                dialog.dismiss();
+                            }
+                        }
+                    });
 
-					final Dialog dialog = new Dialog(context, MResource.getIdByName(context,
-							"style", "huo_sdk_customDialog"));
-					View notcieView = LayoutInflater.from(context)
-							.inflate(MResource.getIdByName(context, "layout", "huo_sdk_dialog_notice"), null);
-					TextView title = (TextView) notcieView.findViewById(MResource.getIdByName(context,
-							"id", "huo_sdk_title_text"));
-					TextView time = (TextView) notcieView.findViewById(MResource.getIdByName(context,
-							"id", "huo_sdk_time_text"));
-					TextView content = (TextView) notcieView.findViewById(MResource.getIdByName(context,
-							"id", "huo_sdk_content_text"));
-					TextView confirm = (TextView) notcieView.findViewById(MResource.getIdByName(context,
-							"id", "huo_sdk_confirm_tv"));
-					title.setText(notice.getTitle());
-					time.setText(notice.getTime());
-					content.setText(notice.getContent());
-					confirm.setOnClickListener(new View.OnClickListener() {
-						@Override
-						public void onClick(View v) {
-							if (dialog != null){
-								dialog.dismiss();
-							}
-						}
-					});
+                    dialog.setCancelable(false);
+                    dialog.setCanceledOnTouchOutside(false);
+                    dialog.setContentView(notcieView);
 
-					dialog.setCancelable(false);
-					dialog.setCanceledOnTouchOutside(false);
-					dialog.setContentView(notcieView);
-
-					if (! dialog.isShowing()){
-						dialog.show();
-					}
-				}
-			});
-		}
-	}
+                    if (!dialog.isShowing()) {
+                        dialog.show();
+                    }
+                }
+            });
+        }
+    }
 
 
 }
